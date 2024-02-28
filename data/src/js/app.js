c9484d4ed2a859035e86ce5c6cfa7b81cdef67ed
KrzyBan95 <33942315+KrzyBan21@users.noreply.github.com>
2024-02-28 01:10:06 +0100

    Dodanie cachowania

--- data/src/js/app.js
+++ data/src/js/app.js
@@ -1,5 +1,9 @@
 let deferredPrompt;
 
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/sw.js").then(function () {
     console.log("Service worker registered!");
@@ -12,3 +16,46 @@ window.addEventListener("beforeinstallprompt", function (event) {
   deferredPrompt = event;
   return false;
 });
+
+// let prom = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve("This is executed when its done");
+//   }, 3000);
+// });
+
+// prom.then(function (val) {
+//   console.log(val);
+// });
+
+// fetch("https://httpbin.org/post", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     messages: "Does it work?",
+//   }),
+// })
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
+
+// fetch("https://httpbin.org/ip")
+//   .then(function (response) {
+//     console.log(response);
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   })
+//   .catch(function (err) {
+//     console.log(err);
+//   });
--- sw.js
+++ sw.js
@@ -1,13 +1,73 @@
+let CACHE_STATIC_NAME = "static-v4";
+let CACHE_DYNAMIC_NAME = "dynamic-v3";
+
 self.addEventListener("install", function (event) {
-  console.log("[Service Worker] Installing service worker...", event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[SW] Precaching App Shell");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/data/src/js/app.js",
+        "/data/src/js/feed.js",
+        "/data/src/js/promise.js",
+        "/data/src/js/fetch.js",
+        "/data/src/js/material.min.js",
+        "/data/src/css/app.css",
+        "/data/src/css/feed.css",
+        "/data/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+      // cache.add("/index.html");
+      // cache.add("/data/src/js/app.js");
+    })
+  );
+
+  // console.log("[Service Worker] Installing service worker...", event);
 });
 
 self.addEventListener("activate", function (event) {
-  console.log("[Service Worker] Activating service worker...", event);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("[SW] Delete old cache");
+            return caches.delete(key); // returns promises
+          }
+        })
+      );
+    })
+  );
+
+  // console.log("[Service Worker] Activating service worker...", event);
   return self.clients.claim();
 });
 
 self.addEventListener("fetch", function (event) {
-  console.log("[Service Worker] fetching somth ...", event);
-    // event.respondWith();
+  // console.log("[Service Worker] fetching somth ...", event);
+  // event.respondWith();
+
+  // console.log(event.request);
+
+  event.respondWith(
+    caches
+      .match(event.request) //even if event.request has nothing it gives then (return null)
+      .then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function (res) {
+              caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+                cache.put(event.request.url, res.clone());
+                return res;
+              });
+            })
+            .catch(function (err) {});
+        }
+      })
+  );
 });
